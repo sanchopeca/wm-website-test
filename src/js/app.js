4b1e941f1aaa15e96c2927c6a54b1d027dc58107
Petar <ilicpetar992@gmail.com>
2021-07-25 20:10:53 +0200

    finished drop-down menu js logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+//scroll logic
+
 const scrollToTop = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
@@ -5,3 +7,12 @@ const scrollToTop = () => {
 const scrollTopBtn = document.querySelector('.arrow-btn');
 
 scrollTopBtn.addEventListener('click', scrollToTop);
+
+//header logic
+
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const header = document.querySelector('header');
+
+hamburgerBtn.addEventListener('click', () => {
+  header.classList.toggle('menu-open')
+})
